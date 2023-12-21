3635ad5d1b28b8b966f7efbda8f3b3f7c0642609
sakil-sagor <sakilsagor76543@gmail.com>
2023-12-21 19:10:48 +0600

    make academic semester validation file

--- src/app/modules/academicSemester/adademicSemester.validation.ts
+++ src/app/modules/academicSemester/adademicSemester.validation.ts
@@ -1,8 +1,17 @@
 import { z } from 'zod';
+import {
+  AcademicSemesterCode,
+  AcademicSemesterName,
+  Months,
+} from './academicSemester.constant';
 
 const createAcdemicSemesterValidationSchema = z.object({
   body: z.object({
-    
+    name: z.enum([...AcademicSemesterName] as [string, ...string[]]),
+    year: z.date(),
+    code: z.enum([...AcademicSemesterCode] as [string, ...string[]]),
+    startMonth: z.enum([...Months] as [string, ...string[]]),
+    endMonth: z.enum([...Months] as [string, ...string[]]),
   }),
 });
 
